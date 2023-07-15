24b57c813ca02434c2376772416ad94f278610d2
YuugouOhno <yuugou.ohno@gmail.com>
2023-07-15 18:47:53 +0900

    コピペの判定

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,31 @@
 import * as vscode from "vscode";
 
+function detectInputLanguage(inputString: string): string {
+    if (/[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}]/u.test(inputString)) {
+        return 'Japanese';
+    } else {
+        return 'NotJapanese';
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	
+	console.log("読み込み完了");
 	let disposable = vscode.commands.registerCommand(
 		"vscode-detect-snippet.helloWorld",
 		() => {
 			vscode.window.showInformationMessage(
 				"Hello World from vscode-detect_snippet!"
 			);
-			console.log("test");
 		}
 	);
 
 	vscode.workspace.onDidChangeTextDocument(event => {
         let changes = event.contentChanges;
-		console.log(changes);
+		if(changes[0].text.length > 1) {
+			if(detectInputLanguage(changes[0].text) === 'NotJapanese'){
+				console.log("コピペしたね！");
+			}
+		}
     });
 
 	context.subscriptions.push(disposable);
