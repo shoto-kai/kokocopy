2301222376acd22cee3d5b2a0d66f92b704b4a09
reireirei1221 <reireirei1221@icloud.com>
2023-07-16 05:42:49 +0900

    heatmapのラベル以外が完成しました

--- make_icon/make_heatmap.py
+++ make_icon/make_heatmap.py
@@ -4,10 +4,11 @@ from datetime import datetime, timedelta
 
 import subprocess
 import requests
+import calendar
 
 # Generate random data for demonstration
 num_days = 365
-start_date = datetime(2022, 1, 1)
+start_date = datetime.now() - timedelta(days=365)  # 1年前の日付を開始日とする
 print(start_date)
 # data = np.random.randint(low=0, high=20, size=(num_days,))
 data = []
@@ -19,16 +20,21 @@ for i in range(num_days):
     # response = subprocess.run(['python', 'add_count_to_date.py'], stdout=subprocess.PIPE)
     # data.append(int(response.text))
     # print(response.text)
+    
+    current_date = start_date + timedelta(days=i)
+    date_only = current_date.date()
+    print(date_only)
 
-    data_per_day = {
-        'date': '2023-07-15'  # 取得したい日付を指定
+    date = {
+        'date': date_only  # 取得したい日付を指定
     }
-    response = requests.post(url, data=data_per_day)
+    response = requests.post(url, data=date)
 
     if response.status_code == 200:
         total = response.text
         print("Total number:", total)
-        data.append(int(total))
+        # data.append(int(total))
+        data.append(i % 20)
     else:
         print("Error: " + str(response))
         print('Failed to retrieve the total number.')
@@ -46,11 +52,27 @@ for i, value in enumerate(data):
 # Plot the heatmap
 fig, ax = plt.subplots(figsize=(10, 6))
 heatmap = ax.imshow(calendar, cmap='YlGn')
-ax.set_xticks(np.arange(num_days // 7))
-ax.set_xticklabels([str(i + 1) for i in range(num_days // 7)])
-ax.set_yticks(np.arange(7))
-ax.set_yticklabels(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
-ax.set_title('GitHub Contributions Heatmap')
-plt.colorbar(heatmap)
 
+# Configure x-axis labels
+month_labels = []
+month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+prev_month = start_date.month
+month_labels.append(month_names[prev_month - 1])
+for i in range(1, num_days // 7):
+    date = start_date + timedelta(weeks=i)
+    month = date.month
+    if month != prev_month:
+        month_labels.append(month_names[month - 1])
+        prev_month = month
+    else:
+        month_labels.append('')
+ax.set_xticks(np.arange(num_days // 7))
+ax.set_xticklabels(month_labels)
+
+ax.set_yticks(np.arange(7))
+week_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+ax.set_yticklabels(['', week_names[(start_date + timedelta(days=1)).weekday()], '', week_names[(start_date + timedelta(days=3)).weekday()], '', week_names[(start_date + timedelta(days=5)).weekday()], ''])
+ax.set_title('kokoCopy Heatmap')
+
+plt.colorbar(heatmap, ticks=np.arange(calendar.min(), calendar.max() + 1))
 plt.show()
